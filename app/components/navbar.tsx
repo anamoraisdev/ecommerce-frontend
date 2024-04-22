e675284fdadc6d50cc6dbedc79bd807e9c108c66
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-04-22 18:03:05 -0300

    creating filter component and product page, filtering products by category and navbar route parameters

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,10 +1,12 @@
 'use client';
 import service from "@/lib/service"
+import Link from "next/link";
+
 import { useEffect, useState } from "react";
 
 const Navbar = () => {
     const [categories, setCategories] = useState<any[]>([]);
-    
+
     const fetchCategories = async () => {
         try {
             const categoriesData = await service.getData("categories");
@@ -16,15 +18,15 @@ const Navbar = () => {
     useEffect(() => {
         fetchCategories();
     }, []);
-
     return (
         <main className="bg-primary w-full h-32 fixed-top flex items-center justify-between" >
             <h1 className="text-white mx-10">LOGOSTORE</h1>
             <section className="flex gap-4 px-10">
                 {categories.map((item) =>
-                    <a href='' className="text-white hover:text-slate-200" key={item.id}>
-                       {item.name}
-                    </a>
+                        <Link href={`/products?category=${item.id}`} key={item.id}
+                         className="text-white hover:text-slate-200">{item.name}
+                    </Link>
+
                 )}
                 <a href='' className="text-white hover:text-slate-200">
                     Fale conosco
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,19 @@
-
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "ecommerce",
-};
+import Navbar from "@/app/components/navbar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-      </Head>
-      <body className={inter.className}>
+      <head>
+        <title>Minha loja</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
         <Navbar />
         {children}
       </body>
