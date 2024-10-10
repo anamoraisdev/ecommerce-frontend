d4483da6a9e61ff3a68a49dee54cbba98e80778b
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-10-09 22:15:44 -0300

    editing card product

--- app/components/cardProduct.tsx
+++ app/components/cardProduct.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart, FaStar } from "react-icons/fa";
 interface Color {
     name: string,
     src: string
@@ -17,17 +18,35 @@ export interface Product {
 }
 
 const CardProduct: React.FC<{ product: Product }> = ({ product }) => {
-    
-    return(
+    const [isFavorite, setIsFavorite] = useState(false)
+
+    return (
         <>
-        <Link href={`product?id=${product.id}`}>
-            <div className="border rounded-lg flex flex-col justify-between items-center w-[12rem] h-[14rem]">
-                <img src={product.image_urls[0]} className="w-[12rem] h-[12rem] rounded-t-lg"/>
-                
-                <h1>{product.name}</h1>
-            
+            <div className=" relative border rounded-lg flex flex-col items-center w-[15rem] h-[21rem]">
+                <Link href={`product?id=${product.id}`}>
+                    <img src={product.image_urls[0]} className="w-[15rem] h-[15rem] rounded-t-lg hover:scale-95" />
+                </Link>
+
+                <button onClick={() => setIsFavorite(!isFavorite)}
+                    className="absolute top-2 right-2 rounded-full bg-white p-4 cursor-pointer">
+                    {isFavorite ? <FaHeart color="red" /> : <FaRegHeart />}
+                </button>
+
+                <div className="flex flex-col items-start px-4 py-2">
+                    <h1>{product.name}</h1>
+                    <div className="flex">
+                        <FaStar color="gray" size={10} />
+                        <FaStar color="gray" size={10} />
+                        <FaStar color="gray" size={10} />
+                        <FaStar color="gray" size={10} />
+                        <FaStar color="gray" size={10} />
+                    </div>
+                    <p className="text-xs font-bold">{`R$${product?.price}`}<span className="text-xs font-normal"> no pix</span> <span className="text-gray-500 text-xs line-through">R$149,99</span></p>
+                    <p className="text-xs">ou <span className="font-bold">R$115,99</span> em até 9x sem juros</p>
+
+                </div>
+
             </div>
-        </Link>
         </>
     )
 }
