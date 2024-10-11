d178a99304609093a2e473d235fd85fc6b856e99
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-10-11 17:51:49 -0300

    creation of the base page and 'my data' page

---
+++ app/profile/cupons/page.tsx
@@ -0,0 +1,14 @@
+import LayoutProfile from "../../components/layoutProfile"
+
+const Coupons= () => {
+    return (
+        <LayoutProfile>
+
+        <div className="flex h-[calc(100vh-128px)]">
+           Meus Coupons
+        </div>
+        </LayoutProfile>
+    )
+}
+
+export default Coupons
